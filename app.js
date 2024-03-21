6074dff2f597b76f583fddaf7047e214e2a66041
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-03-21 16:24:58 -0500

    changes made

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var exoticgem = require('./routes/exoticgem');
 var grid = require('./routes/grid');
-var pick = require('./routes/pick');
+
 
 
 var app = express();
@@ -27,7 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/exoticgem',exoticgem );
 app.use('/grid',grid );
-app.use('/pick',pick );
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
