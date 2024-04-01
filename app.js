760235899dc7747da320dae9f553091a692186f1
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-04-01 17:54:04 -0500

    modified

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var Gems = require("./models/ExoticGem");
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
-let mongoose = require('mongoose');
+if (!connectionString) {
+  console.error("MongoDB connection string is undefined. Please set the MONGO_CON environment variable.");
+  process.exit(1); // Exit the process if connection string is not defined
+}
+mongoose = require('mongoose');
 mongoose.connect(connectionString);
 
 var indexRouter = require('./routes/index');
